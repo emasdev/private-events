45c07cb2ad9330a8c92198f2c21dcf6f5cb2d086
MarilenaRoque <marilenas@id.uff.br>
2020-08-26 19:37:53 -0300

    Build Table to associate Attendances
    - Create model with attributes
    - Set the realtions

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
 	belongs_to :user, foreign_key: 'user_id'
+	has_many :event_attendings, foreign_key: attended_event_id
+	has_many :attendees, through: :event_attendings, source: :attendee
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, uniqueness: true
   has_many :events
+  has_many :event_attendings, foreign_key: attendee_id
+  has_many :attended_events, through: :event_attendings, dependent: :delete_all
 end

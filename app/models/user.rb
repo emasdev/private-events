c677811c813ef2ee94b0b8791e40e3b57ca21443
Emas <emas.dev@gmail.com>
2020-08-26 17:58:50 -0500

    Fix foreign key on Event & User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, uniqueness: true
-  has_many :events
-  has_many :event_attendings, foreign_key: attendee_id
-  has_many :attended_events, through: :event_attendings, dependent: :delete_all
+  has_many :events, class_name: 'Event', foreign_key: 'user_id', dependent: :delete_all
+  has_many :event_attendings, foreign_key: 'attendee_id'
+  has_many :attended_events, through: :event_attendings
 end

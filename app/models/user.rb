856d37f010d297bca2e8676a6b3b1290a66a15d8
MarilenaRoque <marilenas@id.uff.br>
2020-08-27 19:59:40 -0300

    Add presence required to user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
   has_many :events, class_name: 'Event', foreign_key: 'creator_id', dependent: :delete_all
   has_many :event_attendings, foreign_key: 'attendee_id'
   has_many :attended_events, through: :event_attendings
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject do
     described_class.new(name: 'Anything',
-                        user_id: 1)
+                        id: 1)
   end
 
   it 'is valid with valid attributes' do
@@ -11,7 +11,8 @@ RSpec.describe User, type: :model do
   end
 
   it 'is not valid without a name' do
-    subject.title = nil
+    subject.name = nil
     expect(subject).to_not be_valid
   end
+  
 end
